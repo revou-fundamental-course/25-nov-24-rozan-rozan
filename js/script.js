05f8400b69fa634042d46f366c0b64315bf63f83
rozan-rozan <aqilarhozan@gmail.com>
2024-12-05 16:38:08 +0700

    Checkpoint 7

--- js/script.js
+++ js/script.js
@@ -1,31 +1,17 @@
-//------------------------------------------------------------------------------NAVBAR
+/*------------------------------------------------------------------------------NAVBAR----------*/
 
-window.addEventListener("scroll", function(){                                   //  navbar dinamis
+window.addEventListener("scroll", function(){                                   /*  navbar dinamis  */
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
 
-//------------------------------------------------------------------------------HITUNGAN
+/*------------------------------------------------------------------------------HITUNGAN----------*/
 
 function perhitungan(){
-    let jeniskelamin;
-    document.getElementsByName('inputjeniskelamin').forEach(radio => {          //  value jenis kelamin + console log
-        if (radio.checked) {
-            jeniskelamin = (radio.value);
-            console.log("jenis kelamin: ", jeniskelamin);
-        }
-    })
-
-    let berat = document.getElementById("inputberat").value;                    //  deklarasi & ambil input berat,usia,etc
+    let berat = document.getElementById("inputberat").value;                    /*  deklarasi & ambil input berat,usia,etc  */
     let usia = document.getElementById("inputusia").value;
     let tinggi = document.getElementById("inputtinggi").value;
-
-    if (!jeniskelamin) {return;}                                                //  jika form belum diisi sepenuhnya tidak akan melanjutkan proses penghitungan
-    if (!berat) {return;}
-    if (!usia) {return;}
-    if (!tinggi) {return;}
-
-    let penyakit = [                                                            //  array menyimpan penyakit yang akan ditampilkan
+    let penyakit = [
         "Malnutrisi","Osteoporosis","Anemia Defisiensi Zat Besi","Hipotermia",
         "Diabetes","Hipertensi","Sakit jantung","Osteoarthritis",
         "Penyakit Hati","Diabetes tipe 2","Gangguan pernapasan","Kanker"
@@ -35,12 +21,12 @@ function perhitungan(){
     let saran;
     let range;
 
-    let tinggimeter = parseFloat(tinggi) / 100;                                 //  konversi tinggi cm -> meter
+    let tinggimeter = parseFloat(tinggi) / 100;                                 /*  konversi tinggi cm -> meter  */
     
-    let tinggikuadrat = parseFloat(tinggimeter) * parseFloat(tinggimeter);      //  hitung tinggi kuadrat
-    let hasilhitung = parseFloat(berat) / parseFloat(tinggikuadrat);            //  hitung berat/tinggi
+    let tinggikuadrat = parseFloat(tinggimeter) * parseFloat(tinggimeter);      /*  hitung tinggi kuadrat  */
+    let hasilhitung = parseFloat(berat) / parseFloat(tinggikuadrat);            /*  hitung berat/tinggi  */
 
-    if (hasilhitung < 18.5) {                                                   //  menentukan output berdasarkan skor didapat
+    if (hasilhitung < 18.5) {                                                   /*  menentukan output berdasarkan skor didapat  */
         status = "Anda mengalami kekurangan berat badan";
         kategori = "Kekurangan berat badan";
         saran = "Berat badan Anda berada di bawah normal. Penting untuk mengonsumsi makanan yang sehat dan bernutrisi untuk mencapai berat badan ideal. Disarankan untuk berkonsultasi dengan ahli gizi untuk menyusun rencana makan yang sesuai kebutuhan tubuh Anda.";
@@ -69,8 +55,15 @@ function perhitungan(){
         document.getElementById("listpenyakit").innerHTML = "Beberapa penyakit yang berasal dari kegemukan:";
         document.getElementById("outputpenyakit").innerHTML = penyakit.slice(8,12).join(", ");
     }
+
+    document.getElementsByName('inputjeniskelamin').forEach(radio => {          /*  value jenis kelamin + console log  */
+        if (radio.checked) {
+            jeniskelamin = (radio.value);
+            console.log("jenis kelamin: ", jeniskelamin);
+        }
+    })
     
-    console.log("berat: ", berat);                                              //  console log semua value didapat
+    console.log("berat: ", berat);                                              /*  console log semua value didapat  */
     console.log("usia: ", usia);
     console.log("tinggi (cm): ", tinggi);
     console.log("tinggi (m): ", tinggimeter);
@@ -79,33 +72,23 @@ function perhitungan(){
     console.log("status bb: ", status);
     console.log("kategori: ", kategori);
 
-    document.getElementById("outputhasil").innerHTML = hasilhitung.toFixed(1);  //  tampilkan output hitungan & string
+    document.getElementById("outputhasil").innerHTML = hasilhitung.toFixed(1);  /*  tampilkan output hitungan & string  */
     document.getElementById("outputstatus").innerHTML = status;
     document.getElementById("outputkategori").innerHTML = kategori;
     document.getElementById("outputsaran").innerHTML = saran;
     document.getElementById("outputrange").innerHTML = range;
-    document.getElementById("outputjeniskelamin").innerHTML = jeniskelamin;
 }
 
-const form = document.getElementById('forminput');                              //  mencegah auto refresh saat submit ditekan
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-})
-
-//------------------------------------------------------------------------------FUNGSI RESET HASIL
+/*------------------------------------------------------------------------------FUNGSI RESET----------*/
 
 function resetform() {
-    document.getElementById('outputhasil').textContent = '0';                   //  reset skor hasil
-    document.getElementById('outputstatus').textContent = '-';                  //  reset status
-    document.getElementById('outputrange').textContent = '';                    //  reset range skor
-    document.getElementById('outputkategori').textContent = '-';                //  reset kategori
-    document.getElementById('outputsaran').textContent = '';                    //  reset saran
-}
+    document.getElementById("forminput").reset();                               /*  reset form input bb,usia,tinggi,etc  */
 
-/* 
-LOGIKA:
-1. js tidak akan melakukan penghitungan ketika form belum diisi seluruhnya yaitu jenis kelamin, berat, usia dan tinggi
-2. ketika form sudah diisi user dapat melakukan penghitungan dengan menekan button submit yang bertuliskan "Hitung BMI"
-3. hasil skor, range skor, status berat badan, kategori berat badan, saran, dan penyakit akan ditampilkan setelah berhasil melakukan penghitungan
-4. button reset menghapus isi form dengan fungsi html, juga meghapus hasil menggunakan fungsi javascript
-*/
+    document.getElementById("outputhasil").innerHTML = "0";                     /*  reset skor  */
+    document.getElementById("outputstatus").innerHTML = "-";                    /*  reset status  */
+    document.getElementById("outputrange").innerHTML = "";
+    document.getElementById("outputkategori").innerHTML = "-";                  /*  reset kategori  */
+    document.getElementById("outputsaran").innerHTML = "";                      /*  reset saran  */
+    document.getElementById("listpenyakit").innerHTML = "";
+    document.getElementById("outputpenyakit").innerHTML = "";
+}
